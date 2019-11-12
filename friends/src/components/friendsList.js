c95e68f8a7e8ad6a5121e5f7cf116a6a09664764
Tristan Boudreau <tristanboudreau@Tristans-MacBook-Pro.local>
2019-11-12 16:08:08 -0700

    mcp

---
+++ friends/src/components/friendsList.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import api from '../utils/api';
+import FriendForm from './friendsForm'
+
+function FriendsList(){
+	const [friends, setFriends] = useState([])
+	useEffect(()=>{
+		api().get('/friends')
+			.then((res)=>{
+				setFriends(...friends, res.data)
+			})
+			.catch((err)=>{
+				console.log(err)
+			})
+	}, [])
+
+	return(
+		<div className='friends-list'>
+			<h1>Friends!</h1>
+			<FriendForm />
+			{friends.length ? (friends.map((friend)=>{return(
+				<div key={friend.id} className='friend'>
+					<h3>Name: {friend.name}</h3>
+					<p>Age: {friend.age}</p>
+					<p>Email: {friend.email}</p>
+				</div>
+				)})):(<div>No friends huh...? That's okay... nerd.</div>)}
+		</div>
+	)
+}
+
+export default FriendsList; 
